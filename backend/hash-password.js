f06ceb7b0a37f245678eee0c316b36c84e52dca8
Duongyennhi025 <Duongyennhi025@gmail.com>
2026-04-15 15:25:37 +0700

    Fix Vercel SPA routing

---
+++ backend/hash-password.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+
+async function run() {
+  const password = '123456';
+  const hash = await bcrypt.hash(password, 10);
+  console.log('Password:', password);
+  console.log('Hash:', hash);
+}
+
+run();
